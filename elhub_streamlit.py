9d80754cc45178a18f9aa1bce29b53f909a32b25
TorOEkle <117438207+TorOEkle@users.noreply.github.com>
2022-11-04 20:00:58 +0100

    Update elhub_streamlit.py

--- elhub_streamlit.py
+++ elhub_streamlit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.set_page_config(layout="wide", page_title= 'Elhub visualized', page_icon= "lightning")
 import pandas as pd
+from plotly.subplots import make_subplots
 import numpy as np
 import plotly.express as px
 #from PIL import Image
@@ -76,7 +77,12 @@ df_group = df_group.astype({'region':'category',
                 'volum':'int64',
                 'antall_konsumenter': 'int64',
                 'dato': 'datetime64',
-                'gruppe':'category'})
+                'gruppe':'category'})'
+
+## Add prod volume to transmission loss df
+df_prod_volum = df_produksjon.groupby(['dato', 'region'])[['volum']].sum().reset_index()
+df_loss = pd.merge(df_loss, df_prod_volum, how='left', left_on=['dato','region'], right_on=['dato','region'])
+df_loss.rename(columns= {'volum': 'prod_volum'}, inplace= True)
 
 st.write('Kart over hvor de ulike regionene er https://www.nordpoolgroup.com/en/maps/#/nordic')
 options = st.multiselect(
@@ -95,19 +101,24 @@ else:
     df_produksjon_filter = df_produksjon[df_produksjon['region'].isin(options)] # Filter for choosen regions
     df_produksjon_filter =df_produksjon_filter.groupby(['dato', 'produksjonstype'])[['volum']].sum().reset_index() # Aggregate data
     df_loss_filter = df_loss[df_loss['region'].isin(options)] 
-    df_loss_filter = df_loss_filter.groupby(['dato'])[['fysisk_tap','admin_tap']].sum().reset_index() 
+    df_loss_filter = df_loss_filter.groupby(['dato'])[['fysisk_tap','admin_tap','prod_volum']].sum().reset_index() 
+    df_loss_filter['fysisk_tap_andel'] = df_loss_filter['fysisk_tap']/df_loss_filter['prod_volum']
     df_group_filter = df_group[df_group['region'].isin(options)] 
-    df_group_filter = df_group_filter.groupby(['dato','gruppe'])[['volum','antall_konsumenter']].sum().reset_index() 
+    df_group_filter = df_group_filter.groupby(['dato','gruppe'])[['volum','antall_konsumenter']].sum().reset_index()
  
 
 
 st.subheader(f'''Grafer for  {options}''')
 #Prepare plots
-fig_loss = px.line(df_loss_filter, x = 'dato', y= ['fysisk_tap', 'admin_tap'], template= 'plotly_white' , title='Transmissjonstap')
+fig_loss= make_subplots(specs=[[{"secondary_y": True}]])
+fig1 = px.line(df_loss_filter, x = 'dato', y= ['fysisk_tap','admin_tap'], template= 'plotly_white')
+fig2 = px.line(df_loss_filter, x = 'dato', y= ['fysisk_tap_andel'], template= 'plotly_white')
+fig2.update_traces(yaxis="y2",line_color='#DC3912')
 fig_loss.update_layout({
 'plot_bgcolor': 'rgba(0, 0, 0, 0)',
 'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-}, showlegend=False, yaxis_title="Volum [MWh]")
+}, showlegend=False, title_text = 'Transmisjonstap <br><sup>Rød linje er andel av produksjon, akse til høyre</sup>')
+fig_loss.add_traces(fig1.data + fig2.data)
 
 fig_group = px.line(df_group_filter, x = 'dato', y= ['volum'], color= 'gruppe', template= 'plotly_white', title= 'Forbruk per gruppe' )
 fig_group.update_layout({
